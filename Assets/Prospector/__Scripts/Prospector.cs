b24342230bad60f6241ed942f87936d70545c457
Estifferson Peguero <45677818+ePeguero2@users.noreply.github.com>
2022-11-03 20:20:51 -0700

    Error
    Object reference set to an instance of an object

--- Assets/Prospector/__Scripts/Prospector.cs
+++ Assets/Prospector/__Scripts/Prospector.cs
@@ -16,6 +16,10 @@ public class Prospector : MonoBehaviour
 	public float xOffset = 3;
 	public float yOffset = -2.5f;
 	public Vector3 layoutCenter;
+	public Vector2 fsPosMid = new Vector2(0.5f, 0.90f);
+	public Vector2 fsPosRun = new Vector2(0.5f, 0.75f);
+	public Vector2 fsPosMid2 = new Vector2(0.4f, 1.0f);
+	public Vector2 fsPosEnd = new Vector2(0.5f, 0.95f);
 
 	[Header("Set Dynamically")]
 	public Deck deck;
@@ -25,6 +29,7 @@ public class Prospector : MonoBehaviour
 	public CardProspector target;
 	public List<CardProspector> tableau;
 	public List<CardProspector> discardPile;
+	public FloatingScore fsRun;
 
 	void Awake()
 	{
@@ -33,6 +38,7 @@ public class Prospector : MonoBehaviour
 
 	void Start()
 	{
+		Scoreboard.S.score = ScoreManager.SCORE;
 		deck = GetComponent<Deck>();
 		deck.InitDeck(deckXML.text);
 		Deck.Shuffle(ref deck.cards);
@@ -81,8 +87,8 @@ public class Prospector : MonoBehaviour
 			cp = Draw(); // Pull a card from the top (beginning) of the draw Pile
 			cp.faceUp = tSD.faceUp; // Set its faceUp to the value in SlotDef
 			cp.transform.parent = layoutAnchor; // Make its parent layoutAnchor
-												// This replaces the previous parent: deck.deckAnchor, which
-												// appears as _Deck in the Hierarchy when the scene is playing.
+			// This replaces the previous parent: deck.deckAnchor, which
+			// appears as _Deck in the Hierarchy when the scene is playing.
 			cp.transform.localPosition = new Vector3(
 			layout.multiplier.x * tSD.x,
 			layout.multiplier.y * tSD.y,
@@ -218,7 +224,10 @@ public class Prospector : MonoBehaviour
 				MoveToDiscard(target); // Moves the target to the discardPile
 				MoveToTarget(Draw()); // Moves the next drawn card to the target
 				UpdateDrawPile(); // Restacks the drawPile
+				ScoreManager.EVENT(eScoreEvent.draw);
+				FloatingScoreHandler(eScoreEvent.draw);
 				break;
+
 			case eCardState.tableau:
 				// Clicking a card in the tableau will check if it's a valid play
 				bool validMatch = true;
@@ -237,7 +246,10 @@ public class Prospector : MonoBehaviour
 				tableau.Remove(cd); // Remove it from the tableau List
 				MoveToTarget(cd); // Make it the target card
 				SetTableauFaces(); // Update tableau card face-ups
+				ScoreManager.EVENT(eScoreEvent.mine);
+				FloatingScoreHandler(eScoreEvent.mine);
 				break;
+
 		}
 		CheckForGameOver();
 	}
@@ -276,11 +288,16 @@ public class Prospector : MonoBehaviour
 	{
 		if (won)
 		{
-			print("Game Over. You won! :)");
+			//print("Game Over. You won! :)");
+			ScoreManager.EVENT(eScoreEvent.gameWin);
+			FloatingScoreHandler(eScoreEvent.gameWin);
+
 		}
 		else
 		{
-			print("Game Over. You Lost. :(");
+			//print("Game Over. You Lost. :(");
+			ScoreManager.EVENT(eScoreEvent.gameLoss);
+			FloatingScoreHandler(eScoreEvent.gameLoss);
 		}
 		// Reload the scene, resetting the game
 		SceneManager.LoadScene("__Prospector_Scene_0");
@@ -301,6 +318,56 @@ public class Prospector : MonoBehaviour
 		if (c0.rank == 13 && c1.rank == 1) return (true);
 		// Otherwise, return false
 		return (false);
+	}
+	// Handle FloatingScore movement
+	void FloatingScoreHandler(eScoreEvent evt)
+	{
+		List<Vector2> fsPts;
+		switch (evt)
+		{
+			// Same things need to happen whether it's a draw, a win, or a loss
+			case eScoreEvent.draw: // Drawing a card
+			case eScoreEvent.gameWin: // Won the round
+			case eScoreEvent.gameLoss: // Lost the round
+									   // Add fsRun to the Scoreboard score
+				if (fsRun != null)
+				{
+					// Create points for the Bézier curve1
+					fsPts = new List<Vector2>();
+					fsPts.Add(fsPosRun);
+					fsPts.Add(fsPosMid2);
+					fsPts.Add(fsPosEnd);
+					fsRun.reportFinishTo = Scoreboard.S.gameObject;
+					fsRun.Init(fsPts, 0, 1);
+					// Also adjust the fontSize
+					fsRun.fontSizes = new List<float>(new float[] { 28, 36, 4 });
+					fsRun = null; // Clear fsRun so it's created again
+				}
+				break;
+			case eScoreEvent.mine: // Remove a mine card
+								   // Create a FloatingScore for this score
+				FloatingScore fs;
+				// Move it from the mousePosition to fsPosRun
+				Vector2 p0 = Input.mousePosition;
+				p0.x /= Screen.width;
+				p0.y /= Screen.height;
+				fsPts = new List<Vector2>();
+				fsPts.Add(p0);
+				fsPts.Add(fsPosMid);
+				fsPts.Add(fsPosRun);
+				fs = Scoreboard.S.CreateFloatingScore(ScoreManager.CHAIN, fsPts);
+				fs.fontSizes = new List<float>(new float[] { 4, 50, 28 });
+				if (fsRun == null)
+				{
+					fsRun = fs;
+					fsRun.reportFinishTo = null;
+				}
+				else
+				{
+					fs.reportFinishTo = fsRun.gameObject;
+				}
+				break;
+		}
 	}
 }
 	// This section can be commented out; we're working on real layout now
