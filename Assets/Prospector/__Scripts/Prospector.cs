f32a883d75a368d56bb84ce11e79de1d52ed1915
Estifferson Peguero <45677818+ePeguero2@users.noreply.github.com>
2022-11-03 17:29:53 -0700

    Layout Script created

--- Assets/Prospector/__Scripts/Prospector.cs
+++ Assets/Prospector/__Scripts/Prospector.cs
@@ -10,17 +10,21 @@ public class Prospector : MonoBehaviour {
 	static public Prospector 	S;
 
 	[Header("Set in Inspector")]
-	public TextAsset			deckXML;
+	public TextAsset deckXML;
+	public TextAsset layoutXML;
 
 
 	[Header("Set Dynamically")]
-	public Deck					deck;
+	public Deck	deck;
+	public Layout layout;
 
-	void Awake(){
+	void Awake()
+	{
 		S = this;
 	}
 
-	void Start() {
+	void Start() 
+	{
 		deck = GetComponent<Deck> ();
 		deck.InitDeck (deckXML.text);
 	}
